ea00965def87e85cdfd3e2e37dc512c793caddc2
Nick Pozniak <87877391+imwrdo@users.noreply.github.com>
2024-04-06 13:20:31 +0200

    Update ProgramGlowny.c

--- Lab_4/ProgramGlowny.c
+++ Lab_4/ProgramGlowny.c
@@ -3,6 +3,18 @@
 #include <string.h>
 #include <Windows.h>
 #include <tlhelp32.h>
+#include <Psapi.h>
+
+#define MAX_PROCESSES 10 
+
+DWORD processIds[MAX_PROCESSES]; 
+int numProcesses = 0; 
+
+char* get_current_executable_path() {
+    static char path[MAX_PATH];
+    GetModuleFileName(NULL, path, MAX_PATH);
+    return path;
+}
 
 void display_menu() {
     printf("Menu:\n");
@@ -11,19 +23,6 @@ void display_menu() {
     printf("3. Wyswietl liste procesow\n");
     printf("4. Zakoncz program\n");
     printf("Wybierz opcje: ");
-}
-
-void create_new_process() {
-    char exe_path[] = "ProgramPodrzedny.exe";
-    STARTUPINFO structStartupInfo;
-    PROCESS_INFORMATION structProcInfo;
-
-    GetStartupInfo(&structStartupInfo);
-
-    if (!CreateProcess(NULL, exe_path, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &structStartupInfo, &structProcInfo)) {
-        printf("CreateProcess failed (%d).\n", GetLastError());
-        return;
-    }
 }
 
 void stop_process(DWORD processId) {
@@ -39,6 +38,30 @@ void stop_process(DWORD processId) {
     }
 
     CloseHandle(hProcess);
+}
+
+BOOL is_process_from_this_application(DWORD processId) {
+    HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processId);
+    if (hProcess != NULL) {
+        char path[MAX_PATH];
+        if (GetModuleFileNameEx(hProcess, NULL, path, MAX_PATH) > 0) {
+            char* currentExecutablePath = get_current_executable_path();
+            if (strcmp(path, currentExecutablePath) == 0) {
+                CloseHandle(hProcess);
+                return TRUE;
+            }
+        }
+        if (GetModuleFileNameEx(hProcess, NULL, path, MAX_PATH) > 0) {
+            
+            if (strstr(path, "ProgramPodrzedny.exe") != NULL) {
+                CloseHandle(hProcess);
+                return TRUE;
+            }
+        }
+
+        CloseHandle(hProcess);
+    }
+    return FALSE;
 }
 
 void display_process_list() {
@@ -57,12 +80,38 @@ void display_process_list() {
         return;
     }
 
-    printf("Process List:\n");
+    printf("\nProcess List:\n");
     do {
-        printf("Process ID: %lu, Name: %s\n", pe32.th32ProcessID, pe32.szExeFile);
+        if (is_process_from_this_application(pe32.th32ProcessID)) {
+            printf("Process ID: %lu, Name: %s\n", pe32.th32ProcessID, pe32.szExeFile);
+        }
     } while (Process32Next(hSnapshot, &pe32));
 
     CloseHandle(hSnapshot);
+}
+
+void create_new_process() {
+    char exe_path[] = "ProgramPodrzedny.exe";
+    STARTUPINFO structStartupInfo;
+    PROCESS_INFORMATION structProcInfo;
+
+    GetStartupInfo(&structStartupInfo);
+
+    if (!CreateProcess(NULL, exe_path, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &structStartupInfo, &structProcInfo)) {
+        printf("CreateProcess failed (%d).\n", GetLastError());
+        return;
+    }
+
+    processIds[numProcesses++] = structProcInfo.dwProcessId;
+
+    display_process_list();
+    
+    
+    if(is_process_from_this_application(structProcInfo.dwProcessId)) {
+        printf("New process belongs to this application.\n");
+    } else {
+        printf("New process does not belong to this application.\n");
+    }
 }
 
 int main() {
@@ -78,9 +127,10 @@ int main() {
                 create_new_process();
                 break;
             case 2:
-                printf("Enter Process ID to stop: ");
+                printf("\nEnter Process ID to stop: ");
                 scanf("%lu", &processId);
                 stop_process(processId);
+                printf("Process is stopped\n");
                 break;
             case 3:
                 display_process_list();
